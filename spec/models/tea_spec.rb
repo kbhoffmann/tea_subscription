e2232e7cabc22ea3daa588c30899a53939e783b6
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-11 22:13:01 -0500

    Add tests for relationships

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :subscriptions}
+  it { should have_many(:customers).through(:subscriptions)}
 end
