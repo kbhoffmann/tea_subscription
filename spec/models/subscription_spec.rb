11c9e4a17e2f7310728ae550f645a73f9ad67b67
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-12 08:13:20 -0500

    Add tests for defining enum status and validating presence of all attributes

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
   it { should belong_to :tea }
   it { should belong_to :customer }
+  it { should define_enum_for(:status).with_values(active: 0, cancelled: 1) }
+  it { should validate_presence_of :tea_id }
+  it { should validate_presence_of :customer_id }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :status }
+  it { should validate_presence_of :frequency }
+  it { should validate_presence_of :box_quantity }
 end
