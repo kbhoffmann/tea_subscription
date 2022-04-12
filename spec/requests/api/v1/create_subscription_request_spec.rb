d418f1cd6c62c3a0ba47d7fe7b39463a64fdf510
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-12 16:48:01 -0500

    Edit expectations to be correct format and checking for correct values

--- spec/requests/api/v1/create_subscription_request_spec.rb
+++ spec/requests/api/v1/create_subscription_request_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe 'Create Subscription Request' do
                         "tea_id": "#{tea_1.id}",
                         "customer_id": "#{customer_1.id}",
                         "title": "#{tea_1.name}",
-                        "status": "0",
+                        "status": "active",
                         "frequency": "4",
                         "box_quantity": "1",
                       }
 
     post "/api/v1/customers/#{customer_1.id}/subscriptions", params: subscription_1, as: :json
 
-    expect(Subscription.last[:tea_id]).to eq("#{tea_1.id}")
+    expect(Subscription.last[:tea_id]).to eq(tea_1.id)
 
     parsed_response = JSON.parse(response.body, symbolize_names: true)
 
@@ -26,10 +26,10 @@ RSpec.describe 'Create Subscription Request' do
     expect(parsed_response).to be_a(Hash)
     expect(parsed_response[:data][:type]).to eq("subscriptions")
     expect(parsed_response[:data]).to have_key(:id)
-    expect(parsed_response[:data][:attributes][:customer_id]).to eq(1)
-    expect(parsed_response[:data][:attributes][:tea_id]).to eq(1)
-    expect(parsed_response[:data][:attributes][:title]).to eq(["Starry Night"])
-    expect(parsed_response[:data][:attributes][:status]).to eq(0)
+    expect(parsed_response[:data][:attributes][:customer_id]).to eq(customer_1.id)
+    expect(parsed_response[:data][:attributes][:tea_id]).to eq(tea_1.id)
+    expect(parsed_response[:data][:attributes][:title]).to eq("Starry Night")
+    expect(parsed_response[:data][:attributes][:status]).to eq("active")
     expect(parsed_response[:data][:attributes][:frequency]).to eq(4)
     expect(parsed_response[:data][:attributes][:box_quantity]).to eq(1)
   end
