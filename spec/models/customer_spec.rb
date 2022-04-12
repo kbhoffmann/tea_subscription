a52df1657733b518cb10ea6598a0082fe26bd01c
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-12 16:08:44 -0500

    Add test for validation of columns for customer and tea models

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   it { should have_many :subscriptions}
   it { should have_many(:teas).through(:subscriptions)}
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :address }
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -3,4 +3,9 @@ require 'rails_helper'
 RSpec.describe Tea, type: :model do
   it { should have_many :subscriptions}
   it { should have_many(:customers).through(:subscriptions)}
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :temperature }
+  it { should validate_presence_of :brew_time }
+  it { should validate_presence_of :price }
 end
