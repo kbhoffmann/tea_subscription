d3b4e803fcd7ed03181605c63ec79957cfa067e7
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-12 08:16:59 -0500

    Add validations for presence of attribute and enum status

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :tea
   belongs_to :customer
+  validates_presence_of :tea_id
+  validates_presence_of :customer_id
+  validates_presence_of :title
+  validates_presence_of :status
+  validates_presence_of :frequency
+  validates_presence_of :box_quantity
+  enum status: {active: 0, cancelled: 1}
 end
