3c37deb2b8a6caf42780f2bc68d0a9b482ca1eec
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-13 18:54:22 -0500

    Edit test and model to remove unnessary validations for customer_id, tea_id, and status

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,10 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :tea
   belongs_to :customer
-  validates_presence_of :tea_id
-  validates_presence_of :customer_id
+
   validates_presence_of :title
-  validates_presence_of :status
   validates_presence_of :frequency
   validates_numericality_of :frequency, greater_than_or_equal_to: 1
   validates_presence_of :box_quantity
