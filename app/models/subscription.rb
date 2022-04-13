4f0631cfc8eaa5756bf907366ab4fe003e06be12
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-13 11:19:24 -0500

    Add test and validations for frequency and box_quantity to be greater than 1

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,6 +6,8 @@ class Subscription < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :status
   validates_presence_of :frequency
+  validates_numericality_of :frequency, greater_than_or_equal_to: 1
   validates_presence_of :box_quantity
+  validates_numericality_of :box_quantity, greater_than_or_equal_to: 1
   enum status: {active: 0, cancelled: 1}
 end
