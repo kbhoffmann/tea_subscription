9dacb4f3dccab8069a4d1b281abec3b51ba3ac83
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-04-13 12:33:17 -0500

    add numericality validations for temperature, brew_time, and price

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -4,6 +4,9 @@ class Tea < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :temperature
+  validates_numericality_of :temperature, greater_than: 0
   validates_presence_of :brew_time
+  validates_numericality_of :brew_time, greater_than_or_equal_to: 1
   validates_presence_of :price
+  validates_numericality_of :price, greater_than: 0
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe Tea, type: :model do
   it { should validate_presence_of :temperature }
   it { should validate_presence_of :brew_time }
   it { should validate_presence_of :price }
+  it { should validate_numericality_of(:temperature).is_greater_than(0) }
+  it { should validate_numericality_of(:brew_time).is_greater_than_or_equal_to(1) }
+  it { should validate_numericality_of(:price).is_greater_than(0) }
 end
